6a87e3afaa9884cbd4ddecc54377c007c92fb673
Sydney Blom <sydneyblom@#gmail.com>
2019-10-08 14:34:04 -0500

    Step 1 - reducer and intial state

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialTodoState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+      default:
+        return state;
+    }
+};
